07cd06bd344ed6ba9e345c6e95d8d21e58c1b59d
Revati Pandit <revatipandit576@gmail.com>
2025-06-04 15:36:51 +0530

    add API calls

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,16 +1,25 @@
-const Brand = require('../models/brandModel');
-
+const Brand = require("../models/brandModel");
 
 const createBrand = async (req, res) => {
-  console.log(req.body)
-  console.log(req.user.isAdmin)
+  console.log(req.body);
+  console.log(req.user.isAdmin, "Is admin ");
   const { name } = req.body;
+  const image = req.file ? req.file.filename : null;
   try {
-    if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
+    } else {
+      const existingBrand = await Brand.findOne({ where: { name } });
+      console.log(existingBrand, "existingBrand");
+      if (existingBrand) {
+        res.status(200).send({ message: "Brand Already exist" });
+      } else {
+        const newBrand = await Brand.create({ name, image });
+        res
+          .status(200)
+          .send({ message: "Brand Created Successfully", success: true });
+      }
     }
-    const newBrand = await Brand.create({ name });
-    res.status(200).send({ message: "Brand Created Successfully", success: true });
   } catch (error) {
     res.status(500).send({ error: error });
   }
@@ -18,16 +27,21 @@ const createBrand = async (req, res) => {
 
 // http://localhost:7000/api/brand/create
 
-
 const getAllBrands = async (req, res) => {
   try {
-    const brands = await Brand.findAll({ order: [['id', 'ASC']] });
-    res.status(200).send({ brands, success: true });
+    const brands = await Brand.findAll({ order: [["id", "ASC"]] });
+
+    const modifiedBrands = brands.map((brand) => ({
+      id: brand.id,
+      name: brand.name,
+      image: `http://localhost:7000/uploads/${brand.image}`,
+    }));
+
+    res.status(200).send({ brands: modifiedBrands, success: true });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
 };
-
 
 const getBrandByID = async (req, res) => {
   try {
@@ -47,13 +61,15 @@ const updateBrand = async (req, res) => {
   try {
     const { id } = req.params;
     const { name } = req.body;
-  if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
     }
     const [updated] = await Brand.update({ name }, { where: { id } });
 
     if (updated) {
-      res.status(200).send({ message: "Brand updated successfully", success: true });
+      res
+        .status(200)
+        .send({ message: "Brand updated successfully", success: true });
     } else {
       res.status(404).send({ error: "Brand not found", success: false });
     }
@@ -62,32 +78,31 @@ const updateBrand = async (req, res) => {
   }
 };
 
-
-
 const deleteBrand = async (req, res) => {
   try {
     const { id } = req.params;
-    if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
     }
-    const deleted = await Brand.destroy({ where: { id } });  
+    const deleted = await Brand.destroy({ where: { id } });
     if (!deleted) {
       res.status(404).send({ error: "Brand not found" });
     } else {
-      res.status(200).send({ message: "Brand deleted successfully", success: true });
+      res
+        .status(200)
+        .send({ message: "Brand deleted successfully", success: true });
     }
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
 };
 
-
-
 // Export all functions using CommonJS
 module.exports = {
-    createBrand,
-    getAllBrands,
-    getBrandByID,
-    updateBrand,
-    deleteBrand
+  createBrand,
+  getAllBrands,
+  getBrandByID,
+  updateBrand,
+  deleteBrand,
 };
+
