50437d7db1e241f6a5f77c7ce039aed3eb7e1923
Revati Pandit <revatipandit576@gmail.com>
2025-06-04 15:37:44 +0530

    add API calls

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,30 +1,47 @@
-const Category = require('../models/categoryModel')
+const Category = require("../models/categoryModel");
 
-createCategory = async(req,res)=>{
-    try {
-      if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+const createCategory = async (req, res) => {
+  console.log(req.body);
+  console.log(req.user.isAdmin, "Is admin ");
+  const { name } = req.body;
+  const image = req.file ? req.file.filename : null;
+  try {
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
+    } else {
+      const existingCategory = await Category.findOne({ where: { name } });
+      console.log(existingCategory, "existingCategory");
+      if (existingCategory) {
+        res.status(200).send({ message: "Category Already exist" });
+      } else {
+        const newCategory = await Category.create({ name, image });
+        res
+          .status(200)
+          .send({ message: "Category Created Successfully", success: true });
+      }
     }
-    const { name } = req.body;
-    const newCategory = await Category.create({ name });
-    res.status(200).send({ message: "Category Created Successfully", success: true });
-    } catch (error) {
-        res.status(500).send({ error: error });
-    }
-
-}
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
 
 
-getAllCategories = async(req,res)=>{
-   try {
-    categories = await Category.findAll()
-        res.status(200).send({ success: true , categories: categories});
-    } catch (error) {
-        res.status(500).send({ error: error });
-    }
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
 
-}
- 
+    const modifiedCategories = categories.map((category) => ({
+      id: category.id,
+      name: category.name,
+      image: `http://localhost:7000/uploads/${category.image}`,
+    }));
+
+    res.status(200).send({ success: true, categories: modifiedCategories });
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).send({ error: error.message });
+  }
+};
 
 const getCategoryByID = async (req, res) => {
   try {
@@ -33,9 +50,8 @@ const getCategoryByID = async (req, res) => {
     if (!category) {
       res.status(404).send({ error: "Category not found" });
     } else {
-      res.status(200).send({ message: "Category found successfully", success: true });
+      res.status(200).send({ message: "Category found successfully", success: true, category });
     }
-
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
@@ -43,8 +59,8 @@ const getCategoryByID = async (req, res) => {
 
 const updateCategory = async (req, res) => {
   try {
-    if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
     }
     const { id } = req.params;
     const { name } = req.body;
@@ -52,7 +68,9 @@ const updateCategory = async (req, res) => {
     const [updated] = await Category.update({ name }, { where: { id } });
 
     if (updated) {
-      res.status(200).send({ message: "Category updated successfully", success: true });
+      res
+        .status(200)
+        .send({ message: "Category updated successfully", success: true });
     } else {
       res.status(404).send({ error: "Category not found", success: false });
     }
@@ -63,15 +81,17 @@ const updateCategory = async (req, res) => {
 
 const deleteCategory = async (req, res) => {
   try {
-    if(!req.user.isAdmin){
-      res.status(401).send({message:"Not Authorized"})
+    if (!req.user.isAdmin) {
+      res.status(401).send({ message: "Not Authorized" });
     }
     const { id } = req.params;
 
     const deleted = await Category.destroy({ where: { id } });
 
     if (deleted) {
-      res.status(200).send({ message: "Category deleted successfully", success: true });
+      res
+        .status(200)
+        .send({ message: "Category deleted successfully", success: true });
     } else {
       res.status(404).send({ error: "Category not found", success: false });
     }
@@ -80,13 +100,10 @@ const deleteCategory = async (req, res) => {
   }
 };
 
-
-
-
 module.exports = {
-    createCategory,
-    getAllCategories,
-    getCategoryByID,
-    updateCategory,
-    deleteCategory
-}
+  createCategory,
+  getAllCategories,
+  getCategoryByID,
+  updateCategory,
+  deleteCategory,
+};
