211acad3684ff2840f5ad5595a461afb5ccb5d5e
Revati Pandit <revatipandit576@gmail.com>
2025-06-06 10:54:25 +0530

    Update categoryRoute.js

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,7 +5,7 @@ const multerMiddleware = require('../middleware/multer')
 const router = express.Router();
 
 
-router.post('/create',authmiddleware.auth,multerMiddleware.single('image'),CategoryController.createCategory)
+router.post('/create',authmiddleware.auth,multerMiddleware.single('image'),CategoryController.createCategory);
 router.get('/getAllCategories',CategoryController.getAllCategories)
 router.get('/getCategoryByID/:id',CategoryController.getCategoryByID)
 router.put('/updateCategory/:id',authmiddleware.auth,CategoryController.updateCategory)
@@ -14,4 +14,6 @@ router.delete('/deleteCategory/:id',authmiddleware.auth,CategoryController.delet
 
 
 module.exports = router;
+
+
 
